2dccb9e99069a4b26c79cc59c2a6255c114b814a
tianzhankui <arisstz6@gmail.com>
2021-04-05 09:56:10 +0800

    修改draw_pic.py

--- draw_pic.py
+++ draw_pic.py
@@ -1,13 +1,20 @@
 # 数据可视化
 from matplotlib import pyplot as plt
 import pandas as pd
+#plt.rcParams['font.sans-serif']=['SimHei'] #用来正常显示中文标签
+plt.rcParams['axes.unicode_minus']=False #用来正常显示负号
 # 第一步：数据读取
 data = pd.read_csv('Shijiazhuang3Mouth.csv')
 # 第二步：数据处理（由于我们知道文本内容，不存在脏数据，故忽略数据清理步骤）
 data['最高气温'] = data['气温'].str.split('/',expand=True)[0]
 data['最低气温'] = data['气温'].str.split('/',expand=True)[1]
+
 data['最高气温'] = data['最高气温'].map(lambda x:x.replace('℃,',''))
-data['最低气温'] = data['最低气温'].map(lambda x:x.replace('℃,',''))
+data['最低气温'] = data['最低气温'].map(lambda x:x.replace(',',''))
+data['最低气温'] = data['最低气温'].map(lambda x:x.replace('℃',''))
+data['最高气温'] = data['最高气温'].astype("int")
+data['最低气温'] = data['最低气温'].astype("int")
+print(data['最高气温'])
 
 dates = data['日期']
 highs = data['最高气温']
@@ -30,7 +37,6 @@ fig.autofmt_xdate() # 绘制斜的日期标签，避免重叠
 plt.ylabel('气温',fontsize=10)  # y轴显示“气温”，字体大小设置为10
 plt.tick_params(axis='both',which='major',labelsize=10)
 
-# plt.plot(highs,lows,label = '最高气温')
 # 修改刻度
 plt.xticks(dates[::1])  # 由于数据不多，将每天的数据全部显示出来
 
