fbb4e46d28ec8f9ae549c6f0124ca0cb4f7d109d
tianzhankui <arisstz6@gmail.com>
2021-04-23 18:57:58 +0800

    rename wechat.py

--- wechat.py
+++ wechat.py
@@ -1,16 +1,31 @@
 import requests
+import json
 
 class Wechat():
+    def __init__(self, contents):
+        self.contents = contents
 
-    def __init__(self, message):
-        self.message = message
-
-    def wechat_send(self):
-        res_head = "https://sc.ftqq.com/SCU164119T8b7f9e41cc8534affb101298fcc1af6b6045f039b8111.send?text="
-        self.message = res_head + self.message
-        requests.get(self.message)
+    def send(self):
+        wxpush_url = 'http://wxpusher.zjiecode.com/api/send/message'
+        data = {
+                    "appToken":"AT_fIZVgg4T6oGclt3SgloPTGYKH2k2clWX",
+                    "content":self.contents,
+                    "contentType":1,
+                    "uids":[
+                            "UID_rDVj3psx8ufLKqzUVQt4LLrfnvUM"
+                            ],   
+               }
+        
+        ## headers中添加上content-type这个参数，指定为json格式
+        headers = {'Content-Type': 'application/json'}
+         
+        ## post的时候，将data字典形式的参数用json包转换成json格式。
+        res = requests.post(url=wxpush_url, headers=headers, data=json.dumps(data))
+        res_dict = json.loads(res.text)
+        #print(res_dict['success'])
 
 
 if __name__ == '__main__':
-    w = Wechat('石家庄天气晴，气温25℃C，空气质量轻度污染')
-    w.wechat_send()
+    msg = input('请输入您要发送到微信的消息文本：')
+    w = Wechat(msg)
+    w.send()
